d088dbacee1b2f86be4d13b4343d48a98e8e881e
NirushanGopalakrishnan <nirushan.gopalakrishnan@stud.unifi.it>
2023-06-21 20:41:08 +0200

    initial test with main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "Activity.h"
+#include "ActivityList.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
 
+    ActivityList log;
+    Activity activity1({14, 0}, {17, 0}, "titolo", "note");
 
+    // The next day
+
+    Activity activity2({18, 0}, {19, 0}, "titolo", "note");
+    log.addActivity(activity2);
+
+    //log.printActivities();
 }
